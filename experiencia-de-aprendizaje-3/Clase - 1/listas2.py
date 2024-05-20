adac4c1fd5f05fc22adc4df0cbc31033b9fe6663
Marcelo Crisostomo <marcelocrisostomodeveloper@gmail.com>
2024-05-19 23:23:24 -0400

    clase 1

---
+++ experiencia-de-aprendizaje-3/Clase - 1/listas2.py
@@ -0,0 +1,17 @@
+"""T
+Las listas ...
+"""
+
+#
+lista = [1,2,3,4,5,6,7,8,9,10]
+listaVariada=[90, "Python", 3.87]
+print(listaVariada[0])
+print(listaVariada[1])
+print(listaVariada[2])
+# ultimo
+print(listaVariada[-1])
+# modificar
+listaVariada[1]="Python 3"
+#eliminar
+del listaVariada[2]
+print(listaVariada)
